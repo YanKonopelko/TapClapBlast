2c87e22cae0101d2d6bc4930b851b2a479fd42ce
Exnone <komand47@mail.ru>
2026-06-22 10:07:34 +0200

    Build Project

---
+++ build/web-mobile/src/settings.8133c.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scenes/Load.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",resources:"9c0f2",main:"8aa50"}};
